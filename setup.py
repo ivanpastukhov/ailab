6babdda98b81877df4fc80b2e7878cbba78c0ce4
ivanpastukhov <ivan.shepherd@yandex.ru>
2020-06-06 15:27:51 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geolib',
+    version='0.0.1',
+    description='geo lib',
+    url='https://github.com/ivanpastukhov/geof.git',
+    author='Ivan Pastukhov',
+    author_email='ivanpastukhoff@gmail.com',
+    license='unlicense',
+    packages=find_packages(),
+    zip_safe=False
+)

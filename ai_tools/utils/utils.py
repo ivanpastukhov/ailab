8cc151b90d6cecd2fb26a569ab9907e1ff8b32ed
ivanpastukhov <ivan.shepherd@yandex.ru>
2020-08-07 11:25:20 +0300

    добавил ф-цию для отрисовка prc и кщс

--- ai_tools/utils/utils.py
+++ ai_tools/utils/utils.py
@@ -97,7 +97,6 @@ def negative_sampler(question, questions, answers, max_tries=10000, n_rand_samp=
                 raise ValueError('Could not sample negative example after {} tries. More unique questions must be added'.format(
                                  counter))
 
-
 def sampler(questions, answers, pos_frac, random_seed, max_tries=10000):
     if pos_frac > 0.5:
         raise NotImplementedError('Only < 0.5 pos_frac values can be used currently.')
@@ -117,3 +116,42 @@ def sampler(questions, answers, pos_frac, random_seed, max_tries=10000):
         for _ in range(nnegatives):
             yield next(negatives)
 
+def draw_prc_roc(y_test, y_score, recall_threshold, fpr_threshold):
+    from matplotlib.ticker import FormatStrFormatter
+    average_precision = average_precision_score(y_test, y_score)
+    precision, recall, _ = precision_recall_curve(y_test, y_score)
+    fpr, tpr, thr = roc_curve(y_test, y_score, )
+    fig, ax = plt.subplots(1, 2, figsize=(17, 8))
+
+    ax[0].step(recall, precision, color='b', alpha=0.2,
+               where='post')
+    ax[0].fill_between(recall, precision, step='post', alpha=0.2,
+                       color='b')
+    ax[0].grid(True)
+    ax[0].set_xlabel('Recall')
+    ax[0].set_ylabel('Precision')
+    ax[0].set_ylim([0.0, 1.05])
+    ax[0].set_xlim([0.0, 1.0])
+    ax[0].set_yticks(np.arange(0, 1.05, 0.05))
+    ax[0].set_xticks(np.arange(0, 1.05, 0.05))
+    ax[0].set_xticklabels(np.arange(0, 1.05, 0.05), rotation='vertical')
+    ax[0].xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    ax[0].set_title('2-class Precision-Recall curve: AP={0:0.2f}'.format(
+        average_precision))
+
+    lw = 2
+    ax[1].plot(fpr, tpr, color='darkorange')
+    ax[1].plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    ax[1].set_xlim([0.0, 1.0])
+    ax[1].set_ylim([0.0, 1.05])
+    ax[1].grid(True)
+    ax[1].set_yticks(np.arange(0, 1.05, 0.05))
+    ax[1].set_xticks(np.arange(0, 1.05, 0.05))
+    ax[1].set_xticklabels(np.arange(0, 1.05, 0.05), rotation='vertical')
+    ax[1].xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    ax[1].set_xlabel('False Positive Rate')
+    ax[1].set_ylabel('True Positive Rate')
+    ax[1].set_title('Receiver operating characteristic')
+    ax[1].legend(loc="lower right")
+
+    plt.show()
